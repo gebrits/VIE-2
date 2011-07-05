c8dfd35cf1912e41dde32c92ddaafe4b4807e604
Sebastian Germesin <neogermi@googlemail.com>
2011-07-05 14:01:36 +0200

    added new types from schemaOrg

---
+++ src/type/country.js
@@ -0,0 +1,12 @@
+// File:   person.js
+// Author: <a href="mailto:sebastian.germesin@dfki.de">Sebastian Germesin</a>
+//
+
+new VIE2.Type(
+    'Country',  //the id of the mapping 
+    'AdministrativeArea', // the super type
+    [ // attributes
+    ],
+    { //the used namespaces, these can be given here, or placed directly into the HTML document's xmlns attribute.
+    }
+);
